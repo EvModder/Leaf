8a64fbf0bfbb5e754f2415c9c7e0547da2fe740e
EvModder <evmodder@gmail.com>
2022-06-07 08:10:59 +0200

    Misc server media/files

---
+++ silent_run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#while true
+#do
+#BINDIR=$(dirname "$(readlink -fn "$0")")
+#cd "$BINDIR"
+echo "Starting server... Use 'screen -XS leaf quit' to exit"
+echo "Starting server... Use 'screen -r leaf' to open terminal"
+echo "Starting server... Use 'Ctrl-d' to close terminal"
+screen -dmS leaf java --add-modules=jdk.incubator.vector -jar *-1.*jar nogui
+sleep .2
+screen -r leaf
+#echo "Rebooting! Press CTRL-C to stop process"
+#done
