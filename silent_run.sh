248df0d55dbd2f69bc520dcc6ae0f4e1d37c09a1
EvModder <evmodder@gmail.com>
2022-12-22 09:10:47 +0100

    Disable spark profiling

--- silent_run.sh
+++ silent_run.sh
@@ -6,7 +6,7 @@
 echo "Starting server... Use 'screen -XS leaf quit' to exit"
 echo "Starting server... Use 'screen -r leaf' to open terminal"
 echo "Starting server... Use 'Ctrl-d' to close terminal"
-screen -dmS leaf java --add-modules=jdk.incubator.vector -jar *-1.*jar nogui
+screen -dmS leaf java --add-modules=jdk.incubator.vector -DPurpur.IReallyDontWantSpark=true -jar *-1.*jar nogui
 sleep .2
 screen -r leaf
 #echo "Rebooting! Press CTRL-C to stop process"
